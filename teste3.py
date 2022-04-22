ef43007fcc364ce8d07926e9f81b20108db14cef
Lioninsk <brleonbla@hotmail.com>
2022-04-21 23:39:27 -0300

    mudanca_teste3

--- teste3.py
+++ teste3.py
@@ -130,11 +130,14 @@ def lcs(string1, string2):
 
 def lcs_init(string1, string2):
     return lcs(string1 ,string2) / min(len(string1),len(string2))
+
+
+
   
 
 
 
-# print(similiarityBetweenStrings('qtd', 'quantidade')) #sim 46%
+print(similiarityBetweenStrings('qtd', 'quantidade')) #sim 46%
 
 # print(abbreviationDetector('cpf', 'codigo_pessoa_fisica')) #sim 100%
 
